3a8c7c88dbe17b033e9f794dff8101f4e66bdd91
krixhiv21 <valibenchaudhari0@gmail.com>
2025-10-16 02:06:58 +0530

    Add multiplication program in C

---
+++ 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+intmain()
+{
+int a,b,c;
+printf("Enter 1st number: ");
+scanf("%d",&a);
+printf("Enter 2nd number :");
+scanf("%d",&b);
+c=a* b;
+printf("%d * %d = %d", a, b, c);
+return 0;
+}
