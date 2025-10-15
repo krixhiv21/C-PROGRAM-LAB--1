a2203b3ed80012c2a532795bfc238f08f5eecabe
krixhiv21 <valibenchaudhari0@gmail.com>
2025-10-16 02:03:08 +0530

    Add simple C program to sum two numbers

---
+++ 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+int a,b,c;
+printf("Enter 1st number :");
+scanf("%d",&a);
+printf("Enter 2nd number :");
+scanf("%d",&b);
+c=a+b;
+printf("%d + %d = %d", a, b, c);
+return 0;}
