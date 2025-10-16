dca619e78bb938791b767e7767af6ee377728243
krixhiv21 <valibenchaudhari0@gmail.com>
2025-10-16 13:41:25 +0530

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n % 5 == 0 && n % 11 == 0)
+        printf("%d is divisible by both 5 and 11.\n", n);
+    else
+        printf("%d is not divisible by both 5 and 11.\n", n);
+
+    return 0;
+}
